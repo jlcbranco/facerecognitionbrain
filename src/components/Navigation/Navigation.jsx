3104260f575f75060fbdacf70c9d622b05bcfc82
Jenniffer Branco <jlcbranco@gmail.com>
2025-07-11 12:42:42 +0100

    Face reconigition using API and SignIn form

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,31 @@
-export default function Navigation() {
-    return (
-        <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
-        </nav>
-    )
+export default function Navigation({ onRouteChange, isSignedIn }) {
+    if(isSignedIn) {
+        return (
+            <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                <p 
+                    onClick={() => onRouteChange('signin')} 
+                    className='f3 link dim black underline pa3 pointer'
+                >
+                        Sign Out
+                </p>
+            </nav>
+        )
+    } else {
+        return (
+            <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                <p 
+                    onClick={() => onRouteChange('signin')} 
+                    className='f3 link dim black underline pa3 pointer'
+                >
+                        Sign In
+                </p>
+                <p 
+                    onClick={() => onRouteChange('register')} 
+                    className='f3 link dim black underline pa3 pointer'
+                >
+                        Register
+                </p>                                
+            </nav>
+        )
+    }
 }
